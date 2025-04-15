2eca003619c4e50cab300d7ad79f3196c5508702
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-04-15 21:07:01 +0100

    Read Data

--- script/main.js
+++ script/main.js
@@ -2,7 +2,36 @@ var productName = document.getElementById("productName")
 var productPrice = document.getElementById("productPrice")
 var productCategory = document.getElementById("productCategory")
 var productDescription = document.getElementById("productDescription")
+var tableData = document.getElementById("tableData")
 
-function getData(){
-    document.getElementById("data").innerHTML = productName.value + productPrice.value + productCategory.value + productDescription.value
+var productList = []
+
+function addData(){
+    var product = {
+        name: productName.value,
+        price: productPrice.value,
+        category: productCategory.value,
+        desc: productDescription.value
+    }
+    productList.push(product);
+    displayData()
+}
+
+function displayData(){
+    var box = ''
+    for(var i = 0; i < productList.length; i++){
+        box += `
+            <tr>
+                <td>${productList[i].name}</td>
+                <td>${productList[i].price}</td>
+                <td>${productList[i].category}</td>
+                <td>${productList[i].desc}</td>
+                <td>
+                    <button class="btn btn-outline-warning btn-sm">Update</button>
+                    <button class="btn btn-outline-danger btn-sm">Delete</button>
+                </td>
+            </tr>
+        `
+    }
+    tableData.innerHTML = box
 }
