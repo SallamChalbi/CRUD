0dda404dbaeb97521990d285fcacf97542f8820e
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-04-16 18:35:37 +0100

    Update Data

--- script/main.js
+++ script/main.js
@@ -4,7 +4,11 @@ var productCategory = document.getElementById("productCategory")
 var productDescription = document.getElementById("productDescription")
 var tableData = document.getElementById("tableData")
 var searchInput = document.getElementById("searchInput")
+var addBtn = document.getElementById("addBtn")
+var updateBtn = document.getElementById("updateBtn")
+
 var keyStorage = "Products"
+var indexInput = 0
 
 var productList = JSON.parse(localStorage.getItem(keyStorage)) || []
 displayData();
@@ -19,6 +23,36 @@ function addProduct(){
     productList.push(product);
     localStorage.setItem(keyStorage, JSON.stringify(productList))
     displayData()
+    clearForm()
+}
+
+function setData(index){
+    indexInput = index
+
+    productName.value = productList[index].name
+    productPrice.value = productList[index].price
+    productCategory.value = productList[index].category
+    productDescription.value = productList[index].desc
+
+    addBtn.classList.add("d-none")
+    updateBtn.classList.remove("d-none")
+}
+
+function updateProduct(){
+    var product = {
+        name: productName.value,
+        price: productPrice.value,
+        category: productCategory.value,
+        desc: productDescription.value
+    }
+    productList.splice(indexInput, 1, product)
+    localStorage.setItem(keyStorage, JSON.stringify(productList))
+    displayData()
+
+    addBtn.classList.remove("d-none")
+    updateBtn.classList.add("d-none")
+
+    clearForm()
 }
 
 function deleteProduct(element){
@@ -37,7 +71,7 @@ function displayData(){
                 <td>${productList[i].category}</td>
                 <td>${productList[i].desc}</td>
                 <td>
-                    <button class="btn btn-outline-warning btn-sm">Update</button>
+                    <button class="btn btn-outline-warning btn-sm" onclick="setData(${i})">Update</button>
                     <button class="btn btn-outline-danger btn-sm" onclick="deleteProduct(${i})">Delete</button>
                 </td>
             </tr>
@@ -58,7 +92,7 @@ function searchProduct(){
                 <td>${productList[i].category}</td>
                 <td>${productList[i].desc}</td>
                 <td>
-                    <button class="btn btn-outline-warning btn-sm">Update</button>
+                    <button class="btn btn-outline-warning btn-sm" onclick="setData(${i})">Update</button>
                     <button class="btn btn-outline-danger btn-sm" onclick="deleteProduct(${i})">Delete</button>
                 </td>
             </tr>
@@ -66,4 +100,11 @@ function searchProduct(){
         }
     }
     tableData.innerHTML = box
+}
+
+function clearForm(){
+    productName.value = ""
+    productPrice.value = ""
+    productCategory.value = ""
+    productDescription.value = ""
 }
